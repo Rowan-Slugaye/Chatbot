e319833b678573a18ea336c9f6c2d61ecb780eab
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2024-02-17 22:32:52 +0000

    Added some token stuff to fix errors at host for main bot

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import discord
 from discord.ext import commands
 from discord.ui import Button
 
+from Token import TOKEN
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -235,7 +237,7 @@ async def on_message(message):
 
 
 try:
-  token = os.getenv("TOKEN") or ""
+  token = os.getenv("TOKEN") or TOKEN
   if token == "":
     raise Exception("Please add your token to the Secrets pane.")
   client.run(token)
